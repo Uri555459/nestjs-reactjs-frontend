78073ea9182e907a79d28c66e8cb043dcacf418d
Uri <uri555459@gmail.com>
2023-03-01 18:59:11 +0300

    Added plugin generate-react-cli

---
+++ templates/default/TemplateName.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+import styles from './TemplateName.module.scss'
+
+interface TemplateNameProps {}
+
+export const TemplateName: FC<TemplateNameProps> = () => {
+	return <div className={styles.templateName}>TemplateName Component</div>
+}
