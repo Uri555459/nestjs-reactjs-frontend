52f7a62e0a7bdb8b8a5c04d31a43a68efa966b0e
Uri <uri555459@gmail.com>
2023-03-04 12:15:40 +0300

    Added theme providers in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import { FC } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import { AuthRootComponent, Home } from './pages'
+import { ColorModeContext, useMode } from './theme'
 
 import { PrivateRoute } from './utils/router/privateRoute'
 
 export const App: FC = () => {
+	const [theme, colorMode] = useMode()
+
 	return (
-		<div className='App'>
-			<Routes>
-				<Route element={<PrivateRoute />}>
-					<Route path='/' element={<Home />} />
-				</Route>
-				<Route path='login' element={<AuthRootComponent />} />
-				<Route path='register' element={<AuthRootComponent />} />
-			</Routes>
-		</div>
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<div className='App'>
+					<Routes>
+						<Route element={<PrivateRoute />}>
+							<Route path='/' element={<Home />} />
+						</Route>
+						<Route path='login' element={<AuthRootComponent />} />
+						<Route path='register' element={<AuthRootComponent />} />
+					</Routes>
+				</div>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
 	)
 }
